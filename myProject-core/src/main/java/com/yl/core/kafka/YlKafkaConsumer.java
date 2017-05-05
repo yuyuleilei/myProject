7ec990acff992dcfceb24a16b6462a55d9c612cb
Yulei <yulei@zhixindu.com>
2017-05-05 16:43:39 +0800

    kafka改造

--- myProject-core/src/main/java/com/yl/core/kafka/YlKafkaConsumer.java
+++ myProject-core/src/main/java/com/yl/core/kafka/YlKafkaConsumer.java
@@ -5,6 +5,7 @@
  */
 package com.yl.core.kafka;
 
+import com.yl.core.app.WebAppConfig;
 import com.yl.core.bean.CustomerBO;
 import com.zhixindu.commons.utils.JsonUtil;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -14,6 +15,11 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.SerializationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -30,57 +36,62 @@ import java.util.Properties;
  * @date 2017/4/28
  * @description
  */
+@PropertySource(value = {"classpath:/kafka-consumer.properties"})
+@Component
 public class YlKafkaConsumer implements Serializable{
-
     private static final long serialVersionUID = -2516563458415433901L;
     private static final Logger LOG = LoggerFactory.getLogger(YlKafkaConsumer.class);
+    private static Properties properties = null;
+    @Value("${bootstrap.servers}")
+    private String bootstrapServers;
+    @Value("${group.id}")
+    private String groupId;
+    @Value("${key.deserializer}")
+    private String keyDeserializer;
+    @Value("${value.deserializer}")
+    private String valueDeserializer;
 
-
-    public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
+    public String consumer(String topic){
+        String resultString = null;
+        properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 //        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "1000");
 //        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
 //        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "range");
 //      properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY, "roundrobin");
 //        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "10000");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringDeserializer");
-
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
         KafkaConsumer<String,String> kafkaConsumer = new KafkaConsumer<String,String>(properties);
         Collection<String> topics = new ArrayList<>();
-        topics.add("test");
-        topics.add("yltest");
+        topics.add(topic);
         kafkaConsumer.subscribe(topics);
-//      kafkaConsumer.subscribe("*");
-        boolean isRunning = true;
-//        while(isRunning) {
-
-            try {
-                ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
-                if (!records.isEmpty()) {
-                    LOG.info("Got {} messages", records.count());
-                    for (ConsumerRecord<String, String> record : records) {
-                        LOG.info("Message with partition: {}, offset: {}, key: {}, value: {}",
-                                record.partition(), record.offset(), record.key(), JsonUtil.toJsonString(record.value()));
-                        CustomerBO customerBO = JsonUtil.parseBean(record.value(),CustomerBO.class);
-
-                    }
-                } else {
-                    LOG.info("No messages to consume");
+        try {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
+            if (!records.isEmpty()) {
+                LOG.debug("Got {} messages", records.count());
+                for (ConsumerRecord<String, String> record : records) {
+                    LOG.debug("Message with partition: {}, offset: {}, key: {}, value: {}",
+                            record.partition(), record.offset(), record.key(), JsonUtil.toJsonString(record.value()));
+                    resultString =  record.value();
                 }
-            } catch (SerializationException e) {
-                LOG.warn("Failed polling some records", e);
+            } else {
+                LOG.debug("No messages to consume");
             }
-//        }
+        } catch (SerializationException e) {
+            LOG.error("Failed polling some records", e);
+        }
         kafkaConsumer.close();
-
+        return resultString;
     }
 
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(WebAppConfig.class);
+        YlKafkaConsumer ylKafkaConsumer = (YlKafkaConsumer)context.getBean("ylKafkaConsumer");
+        CustomerBO customerBO = JsonUtil.parseBean(ylKafkaConsumer.consumer("yltest"),CustomerBO.class);
+        System.out.println(JsonUtil.toJsonString(customerBO));
 
+    }
 
 }
--- myProject-core/src/main/java/com/yl/core/kafka/YlKafkaProducer.java
+++ myProject-core/src/main/java/com/yl/core/kafka/YlKafkaProducer.java
@@ -5,6 +5,7 @@
  */
 package com.yl.core.kafka;
 
+import com.yl.core.app.WebAppConfig;
 import com.yl.core.bean.CustomerBO;
 import com.zhixindu.commons.utils.JsonUtil;
 import org.apache.kafka.clients.producer.Callback;
@@ -13,6 +14,12 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Properties;
@@ -23,42 +30,53 @@ import java.util.Properties;
  * @date 2017/4/28
  * @description
  */
+@PropertySource(value = {"classpath:/kafka-producer.properties"})
+@Component
 public class YlKafkaProducer implements Serializable{
     private static final long serialVersionUID = 2609371768436008481L;
 
     private static final Logger LOG = LoggerFactory.getLogger(YlKafkaProducer.class);
     private static Properties properties = null;
 
-    static {
-        properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("producer.type", "sync");
-        properties.put("request.required.acks", "1");
-        properties.put("serializer.class", "kafka.serializer.DefaultEncoder");
-        properties.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
-//        properties.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-      properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        properties.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-      properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    }
+    @Value("${bootstrap.servers}")
+    private String bootstrapServers;
+    @Value("${producer.type}")
+    private String producerType;
+    @Value("${request.required.acks}")
+    private String acks;
+    @Value("${serializer.class}")
+    private String serializerClass;
+    @Value("${partitioner.class}")
+    private String partitionerClass;
+    @Value("${key.serializer}")
+    private String keySerializer;
+    @Value("${value.serializer}")
+    private String valueSerializer;
 
-    public void produce(){
+    public void produce(String topic,Object data){
+        properties = new Properties();
+        properties.put("bootstrap.servers", bootstrapServers);
+        properties.put("producer.type", producerType);
+        properties.put("request.required.acks", acks);
+        properties.put("serializer.class", serializerClass);
+        properties.put("partitioner.class", partitionerClass);
+        properties.put("key.serializer", keySerializer);
+        properties.put("value.serializer", valueSerializer);
         long t0 = System.currentTimeMillis();
         KafkaProducer<String,String> kafkaProducer = new KafkaProducer<String,String>(properties);
-//        for (int i = 0; i<10; i++) {
-//            kafkaProducer.send(new ProducerRecord<String,String>("yltest", "key1", "value1:"+i + " test1 data1"));
-//        }
-        CustomerBO customerBO = new CustomerBO();
-        customerBO.setName("余磊");
-        customerBO.setMobile("17091918167");
-        customerBO.setId_no("411524198905111410");
-        kafkaProducer.send(new ProducerRecord<String,String>("yltest", "key", JsonUtil.toJsonString(customerBO)));
+        kafkaProducer.send(new ProducerRecord<String,String>(topic, "key", JsonUtil.toJsonString(data)));
         kafkaProducer.close();
         System.out.println("Time : " + (System.currentTimeMillis() - t0));
     }
 
     public static void main(String [] args){
-        YlKafkaProducer ylKafkaProducer = new YlKafkaProducer();
-        ylKafkaProducer.produce();
+        ApplicationContext context = new AnnotationConfigApplicationContext(WebAppConfig.class);
+        YlKafkaProducer ylKafkaProducer = (YlKafkaProducer)context.getBean("ylKafkaProducer");
+        //生产消息
+        CustomerBO customerBO = new CustomerBO();
+        customerBO.setName("余磊");
+        customerBO.setMobile("17091918167");
+        customerBO.setId_no("411524198905111410");
+        ylKafkaProducer.produce("yltest",customerBO);
     }
 }
